b63aa52e9c0a5cc8c79262bc34c78b412897723d
Boworanan <bowonranan.30@gmail.com>
2022-05-30 16:16:25 +0700

    es6

--- fun104.js
+++ fun104.js
@@ -1,3 +1,5 @@
-function show(){
-    
+function show(message,a,...args){
+    return `${message} ${a}  ${+args[0]+args[1]+args[2]}`
 }
+let sum = show("Hello",10,20,30,40,50,60,70,80,90,100)
+console.log(sum)
